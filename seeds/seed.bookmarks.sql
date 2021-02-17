e6802e72427b58e71d616cb8ca1f484fd93ccae8
keithfilegar <keith.filegar@gmail.com>
2021-02-17 14:07:44 -0800

    create seed script for bookmarks table

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+    ('Google', 'www.google.com', 'search for stuff', 5),
+    ('IGN', 'www.ign.com', 'video game stuff', 4),
+    ('Github', 'www.github.com', 'code stuff', 4),
+    ('CNN', 'www.cnn.com', 'news stuff', 2),
+    ('Thrasher', 'www.thrasher.com', 'skateboard stuff', 5);
